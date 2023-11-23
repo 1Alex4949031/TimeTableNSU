285208abfaa5ac2a2aef87b022133a8654375a2d
1Alex4949031 <a.seleznev@g.nsu.ru>
2023-11-23 16:05:01 +0700

    Footer первоначальный компонент добавлен

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <NavBar/>
   <MainPage/>
+  <FooterComponent/>
 </template>
 
 <style>
@@ -10,4 +11,5 @@
 <script setup>
 import NavBar from "@/components/Nav-Bar.vue";
 import MainPage from "@/components/Home-Page.vue";
+import FooterComponent from "@/components/Footer-Component.vue";
 </script>
---
+++ src/components/Footer-Component.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="container">
+      <p class="footer-text">2023. Не пропускайте важные пары.</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer-text{
+  margin-top: 10px;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 400;
+}
+.footer {
+  border: 1px solid #eaeaea;
+  background-color: #fff;
+  color: black;
+  text-align: center;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+}
+
+@media (max-width: 599px) {
+  .footer-text {
+    font-size: 8px;
+  }
+}
+
+
+@media (min-width: 600px) and (max-width: 899px) {
+  .footer-text {
+    font-size: 10px;
+  }
+}
+
+@media (min-width: 900px) {
+  .footer-text {
+    font-size: 15px;
+  }
+}
+</style>
