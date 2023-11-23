91e091ecbe5e2d23d974668fe3c39456f6284dd8
1Alex4949031 <a.seleznev@g.nsu.ru>
2023-11-23 12:08:58 +0700

    Работа над Nav-Bar.vue

--- src/components/Nav-Bar.vue
+++ src/components/Nav-Bar.vue
@@ -5,62 +5,17 @@ import {openModalAuth} from "@/JS/ModalLogic";
 
 <template>
 
-  <b-navbar sticky="true" toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+  <b-navbar toggleable="lg" type="dark" variant="light">
+    <b-navbar-brand class="text" href="#">TimeTable</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
+      <b-navbar-nav class="custom-navbar-nav">
         <b-nav-item @click="openModalAuth()">Login</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
-      </b-navbar-nav>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
-
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
-
-  <!--
-  <nav>
-    <div class="header-nav-bar">
-        <div class="cell" >
-          Тест1
-        </div>
-        <div class="cell" >
-          Тест2
-        </div>
-        <div class="cell" >
-          Тест3
-        </div>
-        <button class="cell" @click="openModalAuth();console.log('Clicked!')" >
-          Войти
-        </button>
-      <a class="cell" @click="openModalAuth();console.log('Clicked!')">Вход</a>
-    </div>
-  </nav>-->
 
   <div class="modals">
     <LogInModal/>
@@ -68,25 +23,12 @@ import {openModalAuth} from "@/JS/ModalLogic";
 </template>
 
 <style scoped>
-
-.header-nav-bar {
-  animation: slideDown 0.3s ease;
-  position: fixed; /* Фиксированное позиционирование относительно окна браузера */
-  top: 0;
-  left: 0;
-  width: 100%; /* Ширина на весь экран */
-  height: 10%; /* Высота на весь экран */
-  background: rgba(150, 150, 0, 0.4); /* Полупрозрачный фон */
-  z-index: 100; /* Z-индекс для позиционирования поверх других элементов */
-  display: flex; /* Используйте Flexbox для выравнивания содержимого */
-  align-items: center; /* Вертикальное выравнивание содержимого */
-  justify-content: normal;
+.text{
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
+  color: black;
 }
-
-
-.cell {
-  padding: 10px; /* Отступы для каждой ячейки */
-  border: 1px solid black; /* Границы для наглядности */
+.custom-navbar-nav {
+  margin-left: auto;
 }
-
 </style>
