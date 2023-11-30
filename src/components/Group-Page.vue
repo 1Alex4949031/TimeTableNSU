cc9d2db66ab38260e2269d019f3cfe748ef6351e
1Alex4949031 <a.seleznev@g.nsu.ru>
2023-11-30 10:12:53 +0700

    Добавление изначальной странички для групп.

---
+++ src/components/Group-Page.vue
@@ -0,0 +1,26 @@
+<script setup>
+// import {ref, onMounted} from 'vue';
+// import {useRoute} from 'vue-router';
+// import axios from 'axios';
+
+// const route = useRoute();
+// const facultyName = route.params.facultyId;
+// const groups = ref([]);
+
+//onMounted(fetchGroups(facultyName))
+</script>
+
+<template>
+  <div>
+    <h1>Группы факультета</h1>
+    <ul>
+<!--      <li v-for="group in groups" :key="group.id">-->
+<!--        {{ group.name }}-->
+<!--      </li>-->
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
