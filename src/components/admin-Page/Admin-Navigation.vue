949964e388f0678a00f30026511d31aa3fe61bce
1Alex4949031 <a.seleznev@g.nsu.ru>
2024-06-05 22:58:39 +0700

    style: Изменил часть текста на жирный шрифт на Home-Page.vue

--- src/components/admin-Page/Admin-Navigation.vue
+++ src/components/admin-Page/Admin-Navigation.vue
@@ -4,6 +4,16 @@ import {isUserLogin, logOut} from "@/js/axios-auth-request";
 import imageModal from "@/assets/images/imageModal1.png";
 import router from "@/router/router";
 import groupPng from "@/assets/images/group.png";
+import planPng from "@/assets/images/plan.png";
+import roomPng from "@/assets/images/room.png";
+import subjectPng from "@/assets/images/subject.png";
+import teacherPng from "@/assets/images/teacher.png";
+import constraintPng1 from "@/assets/images/constraint1.png";
+import constraintPng2 from "@/assets/images/constraint2.png";
+import tablePng from "@/assets/images/table.png";
+import potentialTablePng1 from "@/assets/images/potential_table1.png";
+import potentialTablePng2 from "@/assets/images/potential_table2.png";
+
 
 function routeToPage(page) {
   router.push(page)
@@ -24,39 +34,39 @@ function routeToPage(page) {
           </b-col>
 
           <b-col md="4" class="navigate-image mb-3">
-            <b-img class="nav-img image-example" :src="groupPng" alt="Картинка 2"></b-img>
+            <b-img class="nav-img image-example" :src="teacherPng" alt="Картинка 2"></b-img>
             <h3 class="centered-text">Учитель</h3>
           </b-col>
 
           <b-col md="4" class="navigate-image mb-3">
-            <b-img class="nav-img image-example" :src="groupPng" alt="Картинка 3"></b-img>
+            <b-img class="nav-img image-example" :src="roomPng" alt="Картинка 3"></b-img>
             <h3 class="centered-text">Комната</h3>
           </b-col>
 
           <b-col md="4" class="navigate-image mb-3">
-            <b-img class="nav-img image-example" :src="groupPng" alt="Картинка 3"></b-img>
+            <b-img class="nav-img image-example" :src="subjectPng" alt="Картинка 3"></b-img>
             <h3 class="centered-text">Предмет</h3>
           </b-col>
 
           <b-col md="4" class="navigate-image mb-3">
-            <b-img class="nav-img image-example" :src="groupPng" alt="Картинка 3"></b-img>
+            <b-img class="nav-img image-example" :src="planPng" alt="Картинка 3"></b-img>
             <h3 class="centered-text">Учебный план</h3>
           </b-col>
 
           <b-col md="4" class="navigate-image mb-3">
-            <b-img class="nav-img image-example" :src="groupPng" alt="Картинка 3"></b-img>
+            <b-img class="nav-img image-example" :src="constraintPng1" alt="Картинка 3"></b-img>
             <h3 class="centered-text">Ограничения</h3>
           </b-col>
         </b-row>
         <h2 class="title ms-1 me-1">Взаимодействие с расписанием</h2>
         <b-row class="mt-1 mx-1 mb-1">
           <b-col md="6" class="navigate-image mb-3">
-            <b-img class="nav-img image-example" :src="groupPng" alt="Картинка 1"></b-img>
+            <b-img class="nav-img image-example" :src="tablePng" alt="Картинка 1"></b-img>
             <h3 class="centered-text">Расписание</h3>
           </b-col>
 
           <b-col md="6" class="navigate-image mb-3">
-            <b-img class="nav-img image-example" :src="groupPng" alt="Картинка 2"></b-img>
+            <b-img class="nav-img image-example" :src="potentialTablePng1" alt="Картинка 2"></b-img>
             <h3 class="centered-text">Потенциальное расписание</h3>
           </b-col>
         </b-row>
@@ -64,12 +74,12 @@ function routeToPage(page) {
         <h2 class="title ms-1 me-1">Удаление</h2>
         <b-row class="mt-1 mx-1 mb-1">
           <b-col md="6" class="navigate-image mb-3">
-            <b-img class="nav-img image-example" :src="groupPng" alt="Картинка 1"></b-img>
+            <b-img class="nav-img image-example" :src="potentialTablePng2" alt="Картинка 1"></b-img>
             <h3 class="centered-text">Параметры расписания</h3>
           </b-col>
 
           <b-col md="6" class="navigate-image mb-3">
-            <b-img class="nav-img image-example" :src="groupPng" alt="Картинка 2"></b-img>
+            <b-img class="nav-img image-example" :src="constraintPng2" alt="Картинка 2"></b-img>
             <h3 class="centered-text">Ограничения</h3>
           </b-col>
         </b-row>
